8bf4ce1b7b0eb59a8e7246d9cf0c9e6d214978de
Sandro <sandrocoelho2023a@gmail.com>
2025-04-13 22:22:44 -0300

    inserindo superusuario e novas telas html

--- airline/flights/views.py
+++ airline/flights/views.py
@@ -2,7 +2,16 @@ from django.shortcuts import render
 from flights.models import Flight, Airport
 
 # Create your views here.
+
+
 def index(request):
     return render(request, "flights/index.html", {
         "flights": Flight.objects.all()
+    })
+
+
+def flight(request, flight_id):
+    flight = Flight.objects.get(id=flight_id)
+    return render(request, "flights/flight.html", {
+        "flight": flight
     })
