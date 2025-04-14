6cdff12b9ae7854c693f38c52128c3833daa9def
Sandro <sandrocoelho2023a@gmail.com>
2025-04-13 21:56:08 -0300

    adicionando visualização html

--- airline/flights/models.py
+++ airline/flights/models.py
@@ -12,8 +12,10 @@ class Airport(models.Model):
 
 
 class Flight(models.Model):
-    origin = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="departures")
-    destination = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="arrivals")
+    origin = models.ForeignKey(
+        Airport, on_delete=models.CASCADE, related_name="departures")
+    destination = models.ForeignKey(
+        Airport, on_delete=models.CASCADE, related_name="arrivals")
     duration = models.IntegerField()
 
     def __str__(self):
